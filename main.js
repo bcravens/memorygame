e4700ffe4bd8ec18f1029b609fe25394ebc0aaeb
Bradley Cravens <bradleycravens@gmail.com>
2016-09-22 14:09:57 -0400

    init files from old computer

---
+++ main.js
@@ -0,0 +1,13 @@
+// represents the cards of the game
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//Checks for equality between two cards
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
