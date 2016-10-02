e9ec4b26861ee5a58670dea9c27b83357e7d8825
Bradley Cravens <bradleycravens@gmail.com>
2016-10-02 11:16:33 -0400

    final push

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-// represents the cards of the game
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//Defines cards variables
+var cards = ['queen', 'queen', 'king', 'king'];
+//Defines cards in play
+var cardsInPlay = [];
 
 //Checks for equality between two cards
-if (cardOne === cardTwo) {
+var isMatch = function(cards) {
+	if (cards [0] === cards [1]) {
 	alert("You found a match!");
-}
-else {
+	} else {
 	alert("Sorry, try again.");
+	}
+}	
+
+//Checks if two cards are in play
+function isTwoCards () {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card')); 
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = '<img src="king.jpg">';
+		} else {
+			this.innerHTML = '<img src="queen.jpg">';
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
 }
+
+//Sets a variable to game-board 
+var board = document.getElementById('game-board');
+
+//Function that contains code for generating cards
+var createCards = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		board.appendChild(cardElement);
+	}
+}
+
+//Fires createCards function so it runs 
+createCards();
